9b5b8181cc40f5dfe953f6781baf8a9e0bfb55ad
BorisC <boris.condori@jalasoft.com>
2022-12-23 18:45:45 -0400

    Add cors

--- models/server.js
+++ models/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
-
+const cors = require('cors');
 class Server {
     constructor(){
 
@@ -20,6 +20,9 @@ class Server {
     middlewares() {
         // Desplegar el directorio publico
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        // Añadiendo cors
+        this.app.use( cors() );
     }
 
     configurationSockets(){
